aedbf93515265811e0dc5525a0e1cae28610a3fb
Nundu <jorge_alberto1997@hotmail.com>
2020-10-05 15:53:28 -0500

    Se termino el proyecto toast-notification

--- toast-notification/app.js
+++ toast-notification/app.js
@@ -1,7 +1,25 @@
-const nav = document.querySelector('#nav');
+const container = document.querySelector('.container');
 const btn = document.querySelector('#btn');
 
 btn.addEventListener('click', () => {
-    nav.classList.toggle('active');
-    btn.classList.toggle('active');
+    createNotification();
 });
+
+function createNotification(){
+    const notification = document.createElement('div');
+    notification.classList.add('toast');
+    notification.style.background = randomColors();
+    if(notification.style.background.includes('black')) notification.style.color = 'white';
+    container.appendChild(notification);
+    notification.innerHTML = 'You clicked there';
+    setTimeout(()=>{
+        notification.remove();
+    },2500);
+}
+
+function randomColors(){
+    const colors = ['red','blue','cyan','yellow','green','black','purple','gray'];
+    let random = Math.random() * (colors.indexOf('red') - (colors.length - 1)) + colors.indexOf('gray');
+    let randomFloor = Math.floor(random);
+    return colors[randomFloor];
+}
