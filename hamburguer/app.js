8f4bda3d71d81ddfa05b590a090d2f05a45fd943
Nundu <jorge_alberto1997@hotmail.com>
2020-10-05 14:46:36 -0500

    Projecto hamburguer terminado. Toast notification folder agregado

---
+++ hamburguer/app.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector('#nav');
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    btn.classList.toggle('active');
+});
